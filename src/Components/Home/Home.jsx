de4274dfe867bbb927b5e52ec22dba3245f640a3
TamaraZalavsky82 <tamaraanabellazaslavsky@gmail.com>
2023-07-12 19:00:37 -0300

    footer

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Description from '../Description/Description';
 import style from '../../Components/Home/Home.module.css'
+import { AiOutlineWhatsApp,AiOutlineFacebook } from "react-icons/ai";
 
 
 export default function Home() {
   return (
     <div className={style.container}>
    <button className={style.button}>Contactanos</button>
+   
+  
+   
+   <footer className={style.footer}>
+   <AiOutlineWhatsApp className={style.whatsapp}/>
+   <AiOutlineFacebook className={style.face}/>
+ <h6 className={style.copy}>
+  copy ViZal@2023
+  </h6>
+   </footer>
     </div>
   );
 }
