5967df2c1becd5f95933eebbc046e4a3e7cd5def
TamaraZalavsky82 <tamaraanabellazaslavsky@gmail.com>
2023-07-25 14:15:50 -0300

    sendmail

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "../../Components/Home/Home.module.css";
-import { AiOutlineWhatsApp, AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineWhatsApp, AiOutlineFacebook, AiOutlineMail } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 export default function Home({ selectedLanguage, onLanguageChange }) {
@@ -9,6 +9,14 @@ export default function Home({ selectedLanguage, onLanguageChange }) {
     en: 'Contact us',
     fr: 'Contactez-nous'
   };
+
+  const handleEmailClick = () => {
+    const email = 'vizalgroup0@gmail.com';
+    const subject = 'Consulta';
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}`;
+
+    window.location.href = mailtoLink;
+  };
   return (
     <div className={style.container}>
       <Link className={style.positionB} to="/contact">
@@ -16,6 +24,7 @@ export default function Home({ selectedLanguage, onLanguageChange }) {
       </Link>
       <div className={style.footer}>
         <h6 className={style.copy}>copy ViZal@2023</h6>
+        <AiOutlineMail className={style.socialIcon} onClick={handleEmailClick} />
         <AiOutlineWhatsApp className={style.socialIcon} />
         <AiOutlineFacebook className={style.socialIcon} />
       </div>
