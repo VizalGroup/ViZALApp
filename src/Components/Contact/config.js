42dc8cbe7c9a61a88b21500d0ea8386b32f2105a
Alejandro Villamayor <avillamayor720@gmail.com>
2023-07-25 22:33:17 -0300

    Se agrego mensaje de exito o error en form, se configuro correctamente variables de entorno para deploy, se corrigieron estilos del nosotros se agrego texto al home descriptivo y Tamaño responsive del texto sobre nosotros para pantallas grandes y chicas diferenciados

---
+++ src/Components/Contact/config.js
@@ -0,0 +1,7 @@
+const emailConfig = {
+  REACT_APP_EMAILJS_SERVICE_ID: process.env.REACT_APP_EMAILJS_SERVICE_ID,
+  REACT_APP_EMAILJS_TEMPLATE_ID: process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+  REACT_APP_EMAILJS_USER_ID: process.env.REACT_APP_EMAILJS_USER_ID,
+};
+
+export default emailConfig;
